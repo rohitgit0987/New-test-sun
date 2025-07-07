f3dd93c8d6fdcd5c2d7bbfca59302e19f001abd9
rohitgit0987 <rohitgit0987@gmail.com>
2025-07-07 17:37:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -762,9 +762,9 @@ async def txt_handler(bot: Client, m: Message):
                 try:
                     os.system(cmd)
                     await bot.send_document(chat_id=channel_id, document=f"{name}.mp4", caption=cc)
-    os.remove(f"{name}.mp4")
-    count += 1
-    return
+                    os.remove(f"{name}.mp4")
+                    count += 1
+                return
                 except Exception as e:
                     await bot.send_message(channel_id, f"❌ Video failed: {str(e)}")
                     count += 1
@@ -785,7 +785,7 @@ async def txt_handler(bot: Client, m: Message):
                     count += 1
                     failed_count += 1
                     continue
-if "visionias" in url:
+           if "visionias" in url:
                 async with ClientSession() as session:
                     async with session.get(url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'Pragma': 'no-cache', 'Referer': 'http://www.visionias.in/', 'Sec-Fetch-Dest': 'iframe', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX2121) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': '"Android"',}) as resp:
                         text = await resp.text()
@@ -1095,7 +1095,7 @@ async def text_handler(bot: Client, m: Message):
                     count += 1
                     failed_count += 1
                     continue
-if "visionias" in url:
+           if "visionias" in url:
                 async with ClientSession() as session:
                     async with session.get(url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'Pragma': 'no-cache', 'Referer': 'http://www.visionias.in/', 'Sec-Fetch-Dest': 'iframe', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX2121) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': '"Android"',}) as resp:
                         text = await resp.text()
